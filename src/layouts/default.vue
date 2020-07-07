347434398b2f0426e894f38a288c53bd59aafb9b
Dijkz <guidelmn@gmail.com>
2020-07-08 03:42:03 +0700

    delete hover

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -17,13 +17,9 @@
                 <nuxt-link
                   :to="{ name: 'member-id', params: { id: users.uid } }"
                   v-if="(users.role == 'member')"
-                  >หน้าใช้งาน</nuxt-link
-                >
+                >หน้าใช้งาน</nuxt-link>
                 <div class="st" v-if="(users.role == 'staff')">
-                  <nuxt-link
-                    :to="{ name: 'staff-id', params: { id: users.uid } }"
-                    >หน้าใช้งาน</nuxt-link
-                  >
+                  <nuxt-link :to="{ name: 'staff-id', params: { id: users.uid } }">หน้าใช้งาน</nuxt-link>
                 </div>
               </li>
               <li v-if="users">
@@ -33,24 +29,17 @@
           </b-navbar-nav>
           <b-navbar-nav class="ml-auto">
             <div class="navbar" v-if="users">
-              <span style="padding: 10px; margin: 0 10px;"
-                >สวัสดีคุณ {{ ' ' + users.name }}</span
-              >
+              <span style="padding: 10px; margin: 0 10px;">สวัสดีคุณ {{ ' ' + users.name }}</span>
               <span
                 v-if="users.role == 'member'"
                 style="padding: 10px; margin: 0 10px;"
-                >{{ ' มี ' + users.coin + ' coin' }}</span
-              >
+              >{{ ' มี ' + users.coin + ' coin' }}</span>
               <!-- มีกล่องจดหมาย BET ด้วย -->
-              <b-button variant="outline-danger" @click="signout"
-                >LOGOUT</b-button
-              >
+              <b-button variant="outline-danger" @click="signout">LOGOUT</b-button>
             </div>
             <div class="navbar" v-else>
               <nuxt-link to="/login">
-                <b-button variant="outline-primary" class="login"
-                  >LOGIN</b-button
-                >
+                <b-button variant="outline-primary" class="login">LOGIN</b-button>
               </nuxt-link>
             </div>
           </b-navbar-nav>
@@ -66,14 +55,11 @@
           </b-navbar-brand>
           <div class="navbar" v-if="users">
             <div class="ttt">
-              <span style="padding: 5px; margin: 0 5px;"
-                >สวัสดีคุณ {{ ' ' + users.name }}</span
-              >
+              <span style="padding: 5px; margin: 0 5px;">สวัสดีคุณ {{ ' ' + users.name }}</span>
               <span
                 v-if="users.role == 'member'"
                 style="padding: 5px; margin: 0 5px;"
-                >{{ ' มี ' + users.coin + ' coin' }}</span
-              >
+              >{{ ' มี ' + users.coin + ' coin' }}</span>
             </div>
           </div>
           <b-navbar-toggle target="navbar-toggle-collapse">
@@ -94,34 +80,26 @@
                     class="textDec"
                     :to="{ name: 'member-id', params: { id: users.uid } }"
                     v-if="(users.role == 'member')"
-                    >หน้าใช้งาน</nuxt-link
-                  >
+                  >หน้าใช้งาน</nuxt-link>
                   <div class="st" v-if="(users.role == 'staff')">
                     <nuxt-link
                       class="textDec"
                       :to="{ name: 'staff-id', params: { id: users.uid } }"
-                      >หน้าใช้งาน</nuxt-link
-                    >
-                  </div></b-nav-item
-                >
-                <b-nav-item>
-                  <div>
-                    <nuxt-link to="/password" class="textDec"
-                      >เปลี่ยนรหัสผ่าน</nuxt-link
-                    >
+                    >หน้าใช้งาน</nuxt-link>
                   </div>
                 </b-nav-item>
                 <b-nav-item>
-                  <a @click="signout" style="color: red;">
-                    LOGOUT
-                  </a>
+                  <div>
+                    <nuxt-link to="/password" class="textDec">เปลี่ยนรหัสผ่าน</nuxt-link>
+                  </div>
+                </b-nav-item>
+                <b-nav-item>
+                  <a @click="signout" style="color: red;">LOGOUT</a>
                 </b-nav-item>
               </div>
               <div v-else>
                 <b-nav-item>
-                  <nuxt-link to="/login" class="textDec">
-                    LOGIN
-                  </nuxt-link>
+                  <nuxt-link to="/login" class="textDec">LOGIN</nuxt-link>
                 </b-nav-item>
               </div>
             </b-navbar-nav>
@@ -208,10 +186,6 @@ body {
   background: white;
   box-shadow: 6px 6px 7px rgba(0, 0, 0, 0.15);
 }
-.HOVER:hover {
-  background: #000;
-}
-
 @media only screen and (max-width: 1050px) {
   /* .ulNav {
     display: block;
